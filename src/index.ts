6f4e6ffdcbe460839b8d1749e726577a286609f1
Gathia <marcellochrisdiantoro@gmail.com>
2026-02-25 04:46:31 +0000

    Mengganti nama mahasiswa

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello(nama : string, umur : number): string{
     return `Selamat Datang ${nama} di PWL, umur ${nama} adalah ${umur}`;
 }
-const mahasiswa = "Andi"
+const mahasiswa = "Marcello"
 const umur: number = 19
 console.log(hello(mahasiswa, umur));
